6377724e9d420fdfb44e11d8151b2f629db15053
shoaib soomro <64408582+soomroshoaib@users.noreply.github.com>
2022-08-31 04:53:10 -0700

    minor change

--- Server.mjs
+++ Server.mjs
@@ -6,32 +6,100 @@ const app = express();
 app.use(express.json())
 app.use(cors())
  let users = []
-
+ function  random(){
+   return Math.floor(Math.random() * 10000)
+ }
 
  app.post('/user',(req,res)=>{
     console.log(req.body)
-    users.push(req.body)
-    res.send("server is working now")
+    let newuser = {
+      id: random(),
+      fullName: req.body.fullName,
+      userName: req.body.userName,
+      password: req.body.password
+    }
+    users.push(newuser)
+    res.send("user is created ")
 
  })
 
+     // single user create 
+ app.get('/user/:userId',(req, res)=>{
+    let userId = req.params.userId;
+    let isFond = false;
+    for(let i = 0; i<users.length; i++){
+      if(users[i].id === userId){
+         res.send(userId[i])
+         isFond = true;
+         break;
+      }
+    }
+    if(!isFond){
+      res.send("user is not found ")
+    }
+
+
+ })
 
  app.get('/user',(req, res)=>{
-    res.send(users)
+   res.send(users)
+})
+ 
+ app.put('/user/:useId',(req, res)=>{
+    req.send("user is modify     ")
+    let userId = req.params.useId
+    let userIndex = -1 
+    for(let i = 0; i<users.length; i++){
+      if(users[i].id === userId){
+         res.send(userId[i])
+         userIndex = i 
+         break;
+      }
+    }
+    if(userIndex === -1){
+      res.send(' user is not found ')
+    }else{
+      if(req.body.fullName){
+         users[userIndex].fullName = req.body.fullName
+      };
+      if(req.body.userName){
+         users[userIndex].userName = req.body.userName
+      }
+      if(users.body.password){
+         users[userIndex].password = req.body.password
+      }
+      res.send(users[userIndex])
+    }
+ })
+ 
+ app.delete('/user/:userId',(req, res)=>{
+    req.send("user is deleted         ")
+
+    let userId = req.params.useId
+    let userIndex = -1 
+    for(let i = 0; i<users.length; i++){
+      if(users[i].id === userId){
+         res.send(userId[i])
+         userIndex = i 
+         break;
+      }
+    }
+    if(userIndex === -1){
+      res.send("user is not found ")
+    }else{
+      users.splice(userIndex , 1)
+      res.send("user is deleted ")
+    }
  })
 
- 
- app.put('/user',(req, res)=>{
-    req.send("user is ofde12345    ")
- })
- 
+
  app.delete('/user',(req, res)=>{
-    req.send("usa 12345        ")
- })
-
+   users = []
+   req.send("usa 12345        ")
+})
 
  const Port = process.env.PORT || 3000;
 
  app.listen(Port,()=>{
-    console.log(`localhost ${Port}`)
+    console.log(` localhost ${Port}`)
  })
